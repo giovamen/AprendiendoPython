f20cf190c1908c0d113cb3fdfe0097e751395c24
giovamen <giovamen727@gmail.com>
2024-03-26 10:08:09 -0400

    3

---
+++ Ejercicio3.py
@@ -0,0 +1,16 @@
+#concatena dos cadenas de texto
+
+
+string1 = "Hello"
+string2 = "World"
+result = string1 + " " + string2
+print(result)
+
+#En este ejemplo, tenemos dos cadenas y las concatenamos usando el operador. También agregamos un espacio entre las dos cadenas usando otro operador.string1string2++También puede utilizar el método para concatenar cadenas. He aquí un ejemplo:.format()
+
+
+
+string1 = "Hello"
+string2 = "World"
+result = "{} {}".format(string1, string2)
+print(result)
