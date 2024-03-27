b6a0f472622934b0a76cd53f2a7141a47dc5e0d5
giovamen <giovamen727@gmail.com>
2024-03-27 07:50:40 -0400

    ejercicio4

---
+++ ejercicio4.py
@@ -0,0 +1,10 @@
+'''
+Ejercicio 4 Crear lista con diferentes elementos e imprimirla
+
+'''
+# Crear una lista con diferentes tipos de elementos
+mi_lista = [42, 'Texto', 3.14, True]
+
+# Imprimir cada elemento de la lista
+for elemento in mi_lista:
+    print(elemento)
